6377471cab0598a32e51d29c3b7d2a63e1bf2bd1
KevinRabbe <rabbekevin@icloud.com>
2026-06-04 03:12:27 +0200

    Add compiler package

---
+++ factorio_optimizer/compiler/__init__.py
@@ -0,0 +1,5 @@
+from factorio_optimizer.compiler.request import OptimizationRequest
+from factorio_optimizer.compiler.result import OptimizationReport
+from factorio_optimizer.compiler.simple_compiler import compile_optimization_request
+
+__all__ = ["OptimizationRequest", "OptimizationReport", "compile_optimization_request"]
